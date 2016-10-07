c12f66ae1c8af8ddf618d877a223f6e1b0e50f82
Julian Roggendorf <xLizrd@users.noreply.github.com>
2016-10-07 19:03:29 +0200

    Changed language detection to the currently installed language instead of the originally installed one. Switched to Windows Application instead of Console Application in order to finally get rid of the console window.

--- Program.cs
+++ Program.cs
@@ -1148,7 +1148,7 @@ class calculator : Form
     private void setCurrentLanguage()
     {
         system_language = 
-            System.Globalization.CultureInfo.InstalledUICulture.ToString().Substring(0, 2);
+            System.Globalization.CultureInfo.CurrentCulture.ToString().Substring(0, 2);
     }
 }
 
