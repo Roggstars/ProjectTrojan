d9f54dafcf38887da21b33180c5b64d5be028a03
Julian Roggendorf <xLizrd@users.noreply.github.com>
2016-10-14 23:51:55 +0100

    Belongs to last commit.

---
+++ Precision.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ProjectTrojan
+{
+    public class OutputPrecision
+    {
+        string currentPrecision;
+        public OutputPrecision ()
+        {
+            currentPrecision = "g4";
+        }
+
+        public string GetCurrentPrecision()
+        {
+            return currentPrecision;
+        }
+
+        public void SetCurrentPrecision(string newPrecision)
+        {
+            currentPrecision = newPrecision;
+        }
+
+        public void SetToNextPrecisionInCycle()
+        {
+            if (currentPrecision != "g8")
+            {
+                int currentPrecisionInt = int.Parse(currentPrecision.Substring(1, 1));
+                currentPrecisionInt += 1;
+                SetCurrentPrecision("g" + currentPrecisionInt.ToString());
+            }
+            else
+                SetCurrentPrecision("g0");
+        }
+    }
+}
+
